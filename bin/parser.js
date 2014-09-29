6f722f0c04d36552734540f10ce9321855c04e6e
kayaelle <kerri@achievery.com>
2014-09-29 18:27:53 -0400

    - Moved the validator code into its own function so that assertions can be validated whether they are using JSON-LD or not
    - Reads from a hosted schema file
    
    - Removed recipe files. Default file is now example-assertion.json

--- bin/parser.js
+++ bin/parser.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
+const http = require('http');
+const https = require('https');
+const request = require('request');
 const fs = require('fs');
 const argv = require('optimist').argv;
 const parse = require('../');
 const input = argv._[0];
-var infile = argv.in||argv.infile;;
+var infile = argv.in||argv.infile;
 
 const JaySchema = require('jayschema');
 const jay = new JaySchema();
@@ -14,6 +17,50 @@ const jaynorm = require('jayschema-error-messages');
 const jsonld = require('jsonld');
 const contexts = require('../contexts.js');
 jsonld.documentLoader = contexts;
+
+function openBadgesValidator(validationUrl,data){ 
+   
+  if (typeof validationUrl != 'string'){ 
+    validationUrl = "https://app.achievery.com/tmp/test-OBI-schema.json";
+  }
+    
+  var options = {
+    url: validationUrl,
+    timeout: 3000
+  };
+   
+  request(options, function(err,res,body) {
+    if (err || res.statusCode != 200) {
+      console.log('Schema Url could not be reached.');
+      process.exit(1);
+    }
+      
+    if (isJson(body)){
+      schema = JSON.parse(body);
+      validateSchema(data,schema);
+    }
+    else {
+      console.log('Schema is not valid JSON');
+      process.exit(1);
+    }     
+  });
+  
+  function validateSchema(data,schema){
+
+    jay.validate(data, schema, function(validationErrs){
+      if (validationErrs){
+        console.log("Schema validation errors follow:");
+        console.log(jaynorm(validationErrs));
+        process.exit(1);
+      } 
+      else{
+        console.log("GREATEST SUCCESS OF THE PEOPLE: VALIDATION OF ASSERTION AGAINST ITS SCHEMA PASSSED WITH NO ERRORS.");
+        process.exit(1);
+      }
+    });
+  }
+}
+
 
 function readAssertion(infile) {
   fs.readFile(infile, 'utf8', function(err, data) {
@@ -30,35 +77,34 @@ function readAssertion(infile) {
          console.log("Invalid (JSON-LD Error): " +err);
          process.exit(1);
        }
-
        // Not JSONLD. Continue to validate against schmema.
        if (expanded.length == 0) {
-         console.log('This is not JSONLD');
+         console.log('This is not JSONLD. Validate against Schema.');
+         openBadgesValidator(null,data);
        }
        // Get the validation link
        else {
         contexts(data["@context"],function(err,contextResult){
-          if (err) throw err;
-          var validationUrl = contextResult.document.validation;
+          if (err) {
+            console.log(err);
+            process.exit(1);
+          }
+          var validationUrl = contextResult.document.validation; 
           if (typeof validationUrl === 'string'){
             console.log("Successfully retrieved the validation URL. It is: " + validationUrl);
+            
+            // Not really sure I get this part. Is this if there is no validation url?
             stillMissingSchema = jay.register(fs.readFileSync('files/test-OBI-schema.json'),validationUrl);
             if (stillMissingSchema.length === 0){
-              jay.validate(data, validationUrl, function(validationErrs){
-                if (validationErrs){
-                  console.log("Schema validation errors follow:");
-                  console.log(jaynorm(validationErrs));
-                } 
-                else{
-                  console.log("GREATEST SUCCESS OF THE PEOPLE: VALIDATION OF ASSERTION AGAINST ITS SCHEMA PASSSED WITH NO ERRORS.");
-                }
-              });
+              console.log('validationUrl '+ validationUrl);
+              openBadgesValidator(validationUrl,data);
+            }
+            else {
+              // is this where extensions come in?
             }
           }
         });
        }
-       //console.log("\n=================== THE EXPANDED RESULTS =================")
-       //console.log(JSON.stringify(expanded,null,"  "));
      });
      
     // Not valid JSON. Return error and exit.
@@ -74,8 +120,8 @@ function readAssertion(infile) {
 (function main() {
   infile = argv.in||argv.infile;
   if (!infile)  {
-    console.log('File not provided. Using default Mojito recipe.');
-    infile = "./files/test-recipe.json";
+    console.log('File not provided. Using default example assertion.');
+    infile = "./files/example-assertion.json";
   }  
   readAssertion(infile);
 })()
